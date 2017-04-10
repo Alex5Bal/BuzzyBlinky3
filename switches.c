ffbeee12f4dbde07dd1756f199cbf8f75fcf7e80
Alex5Bal <abalderrama5@outlook.com>
2017-04-10 01:31:29 -0600

    Update switches.c

--- switches.c
+++ switches.c
@@ -31,6 +31,5 @@ switch_interrupt_handler()
   char p1val = switch_update_interrupt_sense();
   switch_state_down = (p1val & SW1) ? 0 : 1; /* 0 when SW1 is up */
   switch_state_changed = 1;
-  buzzer_init();
-  led_init();
+  led_update();
 }
