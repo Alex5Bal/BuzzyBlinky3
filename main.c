0f9295cf343a8fff00a0ecb0a37700e7ca7a9967
Alex5Bal <abalderrama5@outlook.com>
2017-04-10 18:31:23 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,23 @@
 #include <msp430.h>
 #include <libTimer.h>
 #include "buzzer.h"		/* for setPeriod */
-#include "led.h"
-#include "switches.h"			/* for led_control() */
+#include "led.h"		/* for led_control() */
 
 int
 main()
 {
     configureClocks();		/* setup master oscillator, CPU & peripheral clocks */
-    //buzzer_init();
-    //led_init();
-    switch_init();
+    buzzer_init();
+
+    led_init();
 
     enableWDTInterrupts();	/* enable wd timer */
 
     or_sr(0x18);		/* CPU off, GIE on */
 }
+
+
+
 
 
 
